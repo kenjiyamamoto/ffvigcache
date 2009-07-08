3238d933086da984faaf5f23575d678808a9c115
Rodrigo Lopes <rodvlopes@gmail.com>
2009-07-08 01:12:22 -0300

    corrigindo o arquivo de build xpi

--- create_xpi_file.py
+++ create_xpi_file.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
+import commands
 
 def criar_xpi_file():
     criar_dir_temporario()
     criar_jar()
     criar_xpi_de_fato()
-    remover_dir_temporario()
-    
     
 def criar_dir_temporario():
     execute_cmd('mkdir temp')
 
 def criar_jar():
     execute_cmd('cp -r src/* temp/')
-    execute_cmd('cd temp')
-    execute_cmd('cd chrome')
-    execute_cmd('jar cf ffvigcache.jar content/*')
-    execute_cmd('rm -fr content')
-    execute_cmd('cd ../..')
+    execute_cmd('cd temp/chrome')
+    execute_cmd('cd temp/chrome && jar cf ffvigcache.jar content/*')
+    execute_cmd('rm -fr temp/chrome/content')
 
 def criar_xpi_de_fato():
-    execute_cmd('tar czvf ffvigcache.zip temp/*')
+    execute_cmd('cd temp && zip -r ../ffvigcache.xpi *')
     execute_cmd('rm -rf temp')
 
 def execute_cmd(cmd, verbose=True):
